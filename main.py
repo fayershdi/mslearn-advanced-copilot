b8bd27ff18196c6e82fba52686364e47b4976e3a
fayershdi <fayershd@gmail.com>
2026-05-13 14:17:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def countries():
     return list(data.keys())
 
 
-@app.get('/countries/{country}/{city}/{month}')
+@app.get('/countries/{country}/{cities}/{city}')
 def monthly_average(country: str, city: str, month: str):
-    return data[country][city][month]
+    return data[country][city]
 
 # Generate the OpenAPI schema:
 openapi_schema = app.openapi()
