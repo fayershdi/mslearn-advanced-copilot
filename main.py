deb71600754568a1ec447831358e98f53fdcad4f
fayershdi <fayershd@gmail.com>
2026-05-13 14:18:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def root():
 def countries():
     return list(data.keys())
 
-
 @app.get('/countries/{country}/{city}/{month}')
 def monthly_average(country: str, city: str, month: str):
     return data[country][city][month]
